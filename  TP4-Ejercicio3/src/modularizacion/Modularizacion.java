2a558ebe0d033b1be76ce94dcec0a11165f7e5b2
estidy <estidy@gmail.com>
2021-05-22 16:54:04 -0300

    metodo mostrar menu

---  TP4-Ejercicio3/src/modularizacion/Modularizacion.java
+++  TP4-Ejercicio3/src/modularizacion/Modularizacion.java
@@ -1,10 +1,24 @@
 package modularizacion;
-
+import java.util.Scanner;
 public class Modularizacion {
-
+	
+    static Scanner scan = new Scanner(System.in);
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		mostrarMenu();
+		int opcion = solicitarOpcion();
+		calcularAreaParaOpcion(opcion);
+	}
+	
+	private static void mostrarMenu() {
+		System.out.println("-----------------------------------------------MENU-----------------------------------------------");
+		System.out.println("OPCION 1. Calcular Área del Círculo");
+		System.out.println("OPCION 2. Calcular Área del Cuadrado");
+		System.out.println("OPCION 3. Calcular Área del Rectángulo");
+		System.out.println("OPCION 4. Calcular Área del Triángulo");
+		System.out.println("OPCION 5. Calcular Área del Trapecio");
+		System.out.println("--------------------------------------------------------------------------------------------------");
 	}
 
 }
