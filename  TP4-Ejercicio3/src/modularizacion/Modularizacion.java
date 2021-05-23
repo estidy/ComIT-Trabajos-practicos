893034612293c6c10b60e78e9c971b2fd081ef59
estidy <estidy@gmail.com>
2021-05-22 22:33:41 -0300

    refactorizando y agregando los metodos area del rectangula, triangulo y trapecio

---  TP4-Ejercicio3/src/modularizacion/Modularizacion.java
+++  TP4-Ejercicio3/src/modularizacion/Modularizacion.java
@@ -27,38 +27,26 @@ public class Modularizacion {
 		System.out.println("\033[32m--------------------------------------------------------------------------------------------------");
 	}
 	
-	private static void imprimirMensaje(String msj) {
-		System.out.println(msj);
-	}
-	private static int solicitarValorEntero(String msj) {
-		imprimirMensaje(msj);
-		return(scan.nextInt());
-	}
-	private static double solicitarValorDouble(String msj) {
-		imprimirMensaje(msj);
-		return(scan.nextDouble());
-	}
-	
 	private static boolean validarOpcion(int opcion) {
 		boolean salir = false;
 	     switch (opcion) {
-				case 1: {System.out.println("metodo que calcula el area de un círculo");
+				case 1: {
 				         calcularAreaCirculo();
 		                 break;
 		                }
-				case 2: {System.out.println("metodo que calcula el area de un cuadrado");
+				case 2: {
 				         calcularAreaCuadrado();
 				         break;
 				        }
-				case 3: {System.out.println("metodo que calcula el area de un rectangulo");
+				case 3: {
 				         calcularAreaRectangulo();
 		                 break;
 		                }
-				case 4: {System.out.println("metodo que calcula el area de un triangulo");
+				case 4: {
 				         calcularAreaTriangulo();
 		                 break;
 		                }
-				case 5: {System.out.println("metodo que calcula el area de un trapecio");
+				case 5: {
 				         calcularAreaTrapecio();
 		                 break;
 		                }
@@ -68,39 +56,59 @@ public class Modularizacion {
 				default: System.err.println("Opcion Invalida (opciones validas: 1 - 2 - 3 - 4 -5)");         
 		 }	
 	        return(salir?true:false);
+	}	
+	private static void imprimirMensaje(String msj) {
+		System.out.println(msj);
+	}
+	private static int solicitarValorEntero(String msj) {
+		imprimirMensaje(msj);
+		return(scan.nextInt());
+	}
+	private static double solicitarValorDouble(String msj) {		
+		imprimirMensaje(msj);
+		double valor = scan.nextDouble();
+		if(valor > 0)
+			return (valor);
+		else {
+			imprimirMensaje("\033[31mEl numero debe ser mayor a 0. Intentelo Nuevamente");
+			return(solicitarValorDouble(msj));
+		}
 	}
 	
+	private static void imprimirArea(double area, String figura) {
+		System.out.printf("El área del %s es: %.2f metros cuadrados %n", figura, area);
+	}	
 	
 	private static void calcularAreaCirculo() {
 		double metros = solicitarValorDouble("-Ingrese el radio(expresado en metros)-");
 		scan.nextLine();
-		if(metros>0)
-			System.out.printf("El área es: %.2f %n", Math.PI * Math.pow(metros,2));
-		else {
-			imprimirMensaje("\033[31mEl numero debe ser mayor a 0. Intentelo Nuevamente");
-			calcularAreaCirculo();
-		}
+		imprimirArea((Math.PI * Math.pow(metros,2)),"CÍRCULO" );
 	}
 	
 	private static void calcularAreaCuadrado() {
 		double lado = solicitarValorDouble("-Ingrese el lado(expresado en metros)-");
 		scan.nextLine();
-		if(lado>0)
-			System.out.printf("El área es: %.2f %n", Math.pow(lado,2));
-		else {
-			imprimirMensaje("\033[31mEl numero debe ser mayor a 0. Intentelo Nuevamente");
-			calcularAreaCuadrado();
-		}
+		imprimirArea((Math.pow(lado,2)), "CUADRADO");
 	}
 	
 	private static void calcularAreaRectangulo() {
-		
+		double base = solicitarValorDouble("-Ingrese la base (expresado en metros)-");
+		double altura = solicitarValorDouble("-Ingrese la altura (expresado en metros)-");
+		scan.nextLine();	
+		imprimirArea((base * altura), "RECTÁNGULO");
 	}
 	
 	private static void calcularAreaTriangulo() {
-		
+		double base = solicitarValorDouble("-Ingrese la base (expresado en metros)-");
+		double altura = solicitarValorDouble("-Ingrese la altura (expresado en metros)-");	
+		scan.nextLine();	
+		imprimirArea( ((base * altura)/2), "TRIÁNGULO");
 	}
     private static void calcularAreaTrapecio() {
-		
+    	double baseMenor = solicitarValorDouble("-Ingrese la base menor (expresado en metros)-");
+		double baseMayor = solicitarValorDouble("-Ingrese la base mayor (expresado en metros)-");	
+		double altura = solicitarValorDouble("-Ingrese la altura (expresado en metros)-");	
+		scan.nextLine();	
+		imprimirArea( (((baseMayor + baseMenor)/2) * altura), "TRAPECIO");
 	}
 }
