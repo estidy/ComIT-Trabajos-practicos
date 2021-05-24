3c9bba752fd8b1f50b826f6b1459b35d30a67807
estidy <estidy@gmail.com>
2021-05-23 21:25:03 -0300

    definiendo la estructura del main: se declararon dos arreglos uno de string, para almacenar los nombres y apellido de los alumnos; y otro arreglo de tipo double, para almacenar las notas de los alumnos: se creó un metodo de inicializacion para cada vector.

---  TP5-Ejercicio4/src/arreglos_unidimensionales/Arreglos_Unidimensionales.java
+++  TP5-Ejercicio4/src/arreglos_unidimensionales/Arreglos_Unidimensionales.java
@@ -1,10 +1,32 @@
 package arreglos_unidimensionales;
-
+import java.util.Scanner;
+import java.util.Random;
 public class Arreglos_Unidimensionales {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		final int CANT_ALUMNOS = 70;	
+		String[] nombres_alumnos = new String[CANT_ALUMNOS];
+		double[] notas_alumnos = new double[CANT_ALUMNOS];
+		nombres_alumnos = inicializarArregloNombres(CANT_ALUMNOS, nombres_alumnos);
+		notas_alumnos = inicializarArregloNotas(CANT_ALUMNOS, notas_alumnos);
+		imprimirAlumnos(nombres_alumnos, notas_alumnos);
 	}
-
+	private static String[] inicializarArregloNombres(final int CANT_ALUMNOS, String[]nombres_alumnos) {
+		for (int i = 0; i < nombres_alumnos.length; i++) {
+			nombres_alumnos[i] = "Alumno" + 1;
+		}
+		return(nombres_alumnos);
+	}
+	
+	private static double[] inicializarArregloNotas(final int CANT_ALUMNOS, double[]notas_alumnos) {
+		for (int i = 0; i < notas_alumnos.length; i++) {
+			notas_alumnos[i] = (Math.random() * (10 - 1) + 1);
+		}
+		return(notas_alumnos);
+	}
+	
+	public static void imprimirAlumnos(String[]nombres_alumnos, double[]notas_alumnos) {
+		
+	}
 }
