3f956fcd71c938ba0ad27baff62078175c821d92
estidy <estidy@gmail.com>
2021-05-23 23:46:39 -0300

    restructurando el maim. definiendo el metodo imprimirAlumnosQueSuperanPromedio y calcularPromedioNotas

---  TP5-Ejercicio4/src/arreglos_unidimensionales/Arreglos_Unidimensionales.java
+++  TP5-Ejercicio4/src/arreglos_unidimensionales/Arreglos_Unidimensionales.java
@@ -1,21 +1,19 @@
 package arreglos_unidimensionales;
-import java.util.Scanner;
 import java.util.Random;
 public class Arreglos_Unidimensionales {
 	
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		final int CANT_ALUMNOS = 70;	
 		String[] nombres_alumnos = new String[CANT_ALUMNOS];
 		double[] notas_alumnos = new double[CANT_ALUMNOS];
 		nombres_alumnos = inicializarArregloNombres(CANT_ALUMNOS, nombres_alumnos);
 		notas_alumnos = inicializarArregloNotas(CANT_ALUMNOS, notas_alumnos);
-		imprimirAlumnos(nombres_alumnos, notas_alumnos);
+		double promedio_notas = calcularPromedioNotas(notas_alumnos);
+		imprimirAlumnosQueSuperanPromedio(nombres_alumnos, notas_alumnos, promedio_notas);		
 	}
 	private static String[] inicializarArregloNombres(final int CANT_ALUMNOS, String[]nombres_alumnos) {
-		for (int i = 0; i < nombres_alumnos.length; i++) {
-			nombres_alumnos[i] = "Alumno" + 1;
-		}
+		for (int i = 0; i < nombres_alumnos.length; i++) 
+			nombres_alumnos[i] = "Alumno " + (i+1);
 		return(nombres_alumnos);
 	}
 	
@@ -26,7 +24,24 @@ public class Arreglos_Unidimensionales {
 		return(notas_alumnos);
 	}
 	
-	public static void imprimirAlumnos(String[]nombres_alumnos, double[]notas_alumnos) {
-		
+	private static double calcularPromedioNotas(double[]notas_alumnos) {
+		double suma = 0;
+		for (int i = 0; i < notas_alumnos.length; i++) 
+			 suma += notas_alumnos[i];
+		return(suma /notas_alumnos.length);		
+	}
+	
+	public static void imprimirAlumnosQueSuperanPromedio(String[]nombres_alumnos, double[]notas_alumnos, double promedio_notas) {
+		System.out.printf("\t\tALUMNOS QUE SUPERAN LA NOTA PROMEDIO: %.2f %n", promedio_notas);
+		System.out.println("***********************************************************************************");
+		System.out.println("\t\tAPELLIDO Y NOMBRE \t\t|\t\t NOTA");
+		System.out.println("***********************************************************************************");
+		for (int i = 0; i < notas_alumnos.length; i++) {
+			if(notas_alumnos[i] > promedio_notas) {
+				System.out.printf("\tApellido y Nombre: %s \t\t|\t\t %.2f %n", nombres_alumnos[i], notas_alumnos[i]);
+			    System.out.println("-----------------------------------------------------------------------------------");
+			}
+		}
+		System.out.println("***********************************************************************************");
 	}
 }
